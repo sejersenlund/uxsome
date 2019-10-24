0f2d8f374931bfce9c9c37d96f0f567d8b43639f
Jeppe Lund <sejersenlund@hotmail.dk>
2019-10-24 11:57:42 +0200

    Opdateret mappeopsætning

---
+++ js/js.js
@@ -0,0 +1,23 @@
+// JavaScript Document
+
+/*function  displayNextImage() {
+var x;
+
+If (x<imageArray.length) {
+	x=x+1;
+	document.getElementById("imagediv").style.backgroundImage=images[x];
+} 
+else {
+	x=0;
+	document.getElementById("imagediv").style.backgroundImage=images[x];
+}
+
+else {
+	x=false;
+	document.getElementById("imagediv").style.backgroundImage=images[x];
+}
+
+var imageArray=new Array();
+images[0] = "../images/agfpostercold.png";
+images[1] = "../images/agfposterwarm.png";
+images[2] = "../images/agfposter.png";*/
