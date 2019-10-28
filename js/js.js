9a7848adcd918a7325936567f4f2ed3161d4e88d
Jeppe Lund <sejersenlund@hotmail.dk>
2019-10-28 12:36:19 +0100

    slider rettet til 3 punkter

--- js/js.js
+++ js/js.js
@@ -21,3 +21,15 @@ var imageArray=new Array();
 images[0] = "../images/agfpostercold.png";
 images[1] = "../images/agfposterwarm.png";
 images[2] = "../images/agfposter.png";
+	
+
+	
+let findPlayer = new Array [0, 1, 2, 3, 4]
+
+findPlayer[1] = document.getElementById("sliderone");
+findPlayer[2] = document.getElementById("sliderone");
+findPlayer[3] = document.getElementById("sliderone");
+
+if ("sliderOne" === 1) {
+	findPlayer = Array [1]
+}
