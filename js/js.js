4986d9cb6b951a6a59e1022c2029e64013c219f9
Anders Kunnerup <kunnerup16@hotmail.com>
2019-10-28 13:06:19 +0100

    Nyheder

---
+++ js/js.js
@@ -0,0 +1,35 @@
+// JavaScript Document
+
+function  displayNextImage() {
+var x;
+
+if (x<imageArray.length) {
+	x=x+1;
+	document.getElementById("imagediv").style.backgroundImage=images[x];
+} 
+else if {
+	x=0;
+	document.getElementById("imagediv").style.backgroundImage=images[x];
+}
+
+else if {
+	x=false;
+	document.getElementById("imagediv").style.backgroundImage=images[x];
+}
+
+var imageArray=new Array();
+images[0] = "../images/agfpostercold.png";
+images[1] = "../images/agfposterwarm.png";
+images[2] = "../images/agfposter.png";
+	
+
+	
+let findPlayer = new Array [0, 1, 2, 3, 4]
+
+findPlayer[1] = document.getElementById("sliderone");
+findPlayer[2] = document.getElementById("sliderone");
+findPlayer[3] = document.getElementById("sliderone");
+
+if ("sliderOne" === 1) {
+	findPlayer = 
+}
