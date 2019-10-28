cd52d89a25446b049234097805d0407c59bbc383
Jeppe Lund <sejersenlund@hotmail.dk>
2019-10-28 13:36:24 +0100

    edited if/else

--- js/js.js
+++ js/js.js
@@ -7,12 +7,12 @@ if (x<imageArray.length) {
 	x=x+1;
 	document.getElementById("imagediv").style.backgroundImage=images[x];
 } 
-else if {
+else if (x<imageArray.length) {
 	x=0;
 	document.getElementById("imagediv").style.backgroundImage=images[x];
 }
 
-else if {
+else if (x<imageArray.length) {
 	x=false;
 	document.getElementById("imagediv").style.backgroundImage=images[x];
 }
@@ -29,12 +29,12 @@ let findPlayer = new Array [0, 1, 2, 3, 4]
 findPlayer[1] = document.getElementById("sliderone").value;
 findPlayer[2] = document.getElementById("slidertwo").value;
 findPlayer[3] = document.getElementById("sliderthree").value;
-findPlayer[1] = document.getElementById("sliderfour").value;
+findPlayer[4] = document.getElementById("sliderfour").value;
 
-if (“findPlayer[1]" === 1) {
-	return = "Aleksandar Jovanovic"
+if (findPlayer[1] === 1) {
+	return "Aleksandar Jovanovic"
 } else if (findPlayer[2] === 2) {
-	return = "Niklas Backman"
+	return "Niklas Backman"
 } else if (findPlayer[3] === 3) {
-	return = "Mustapha Bundu"
+	return "Mustapha Bundu"
 }
