c7e0b931dda934c036124a0eb58e6889be0f197e
Jeppe Lund <sejersenlund@hotmail.dk>
2019-10-31 12:22:26 +0100

    FINAL

--- js/js.js
+++ js/js.js
@@ -1,27 +1,5 @@
 // JavaScript Document
 
-function  displayNextImage() {
-var x;
-
-if (x<imageArray.length) {
-	x=x+1;
-	document.getElementById("imagediv").style.backgroundImage=images[x];
-} 
-else if (x<imageArray.length) {
-	x=0;
-	document.getElementById("imagediv").style.backgroundImage=images[x];
-}
-
-else if (x<imageArray.length) {
-	x=false;
-	document.getElementById("imagediv").style.backgroundImage=images[x];
-}
-
-var imageArray=new Array();
-images[0] = "../images/agfpostercold.png";
-images[1] = "../images/agfposterwarm.png";
-images[2] = "../images/agfposter.png";
-	
 /*Weather API*/
 
 function hentData(url, callback_Funktion) {
@@ -42,8 +20,8 @@ function visData(jsonData) {
     var divIndhold = "";
 
     divIndhold += '<p>Det er' +
-        + jsonElementer.main.temp + ' og det blæser ' +
-        jsonElementer.wind.speed ;
+        + jsonElementer.main.temp + ' grader og det blæser ' +
+        jsonElementer.wind.speed + 'm/sek';
 
     document.getElementById("indhold").innerHTML = divIndhold;
 
@@ -54,24 +32,27 @@ hentData("http://api.openweathermap.org/data/2.5/weather?q=Aarhus,dk&APPID=2fb0a
 /*Dataanalyse*/
 	
 function showplayer() {
-	
-let findPlayer = new Array (0, 1, 2, 3, 4)
 
-findPlayer[1] = document.getElementById("sliderone").value;
-findPlayer[2] = document.getElementById("slidertwo").value;
-findPlayer[3] = document.getElementById("sliderthree").value;
-findPlayer[4] = document.getElementById("sliderfour").value;
+    let findPlayer = new Array(0, 1, 2, 3, 4);
 
-if (findPlayer[1]===1 && findplayer[2]===1 && findPlayer[3]===3 && findplayer[4]===1) {
-	return "Aleksandar Jovanovic";
-} else if (findPlayer[1]===1 && findplayer[2]===3 && findPlayer[3]===3 && findplayer[4]===1) {
-	return "Niklas Backman"
-} else if (findPlayer[1]===3 && findplayer[2]===3 && findPlayer[3]===1 && findplayer[4]===3) {
-	return "Mustapha Bundu"
-} else if (findPlayer[1]===3 && findplayer[2]===3 && findPlayer[3]===1 && findplayer[4]===1) {
-	return "Mustafa Amini"
-} 
+    findPlayer[1] = parseInt(document.getElementById("sliderone").value);
+    findPlayer[2] = parseInt(document.getElementById("slidertwo").value);
+    findPlayer[3] = parseInt(document.getElementById("sliderthree").value);
+    findPlayer[4] = parseInt(document.getElementById("sliderfour").value);
+
+    if (findPlayer[1] === 1 && findPlayer[2] === 1 && findPlayer[3] === 3 && findPlayer[4] === 1) {
+        return "jovanovic.html";
+    } else if (findPlayer[1] === 1 && findPlayer[2] === 3 && findPlayer[3] === 3 && findPlayer[4] === 1) {
+        return "backman.html";
+    } else if (findPlayer[1] === 3 && findPlayer[2] === 3 && findPlayer[3] === 1 && findPlayer[4] === 3) {
+        return "bundu.html";
+    } else if (findPlayer[1] === 3 && findPlayer[2] === 3 && findPlayer[3] === 1 && findPlayer[4] === 1) {
+        return "amini.html";
+    } else {
+        return "index.html"; //Fallback hvis ingen kombination passer
+    }
 }
+
 	
 /*WeatherAPI*/
 	
@@ -93,12 +74,19 @@ function visData(jsonData) {
     var divIndhold = "";
 
     divIndhold += '<p>På stadion er der ' +
-        + jsonElementer.main.temp + ' og det blæser ' +
+        +jsonElementer.main.temp + ' grader og ' +
         jsonElementer.wind.speed +
-        '</p>';
+        ' m/sek</p>';
 
     document.getElementById("indhold").innerHTML = divIndhold;
 
 }
 
 hentData("http://api.openweathermap.org/data/2.5/weather?q=Aarhus,dk&APPID=2fb0a6717d1db32dc445454559a4f371&units=metric", visData);
+
+
+
+document.getElementById("tjekSpiller").addEventListener('click', function () {
+    window.location.href = showplayer();
+});
+
